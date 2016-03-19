e4df287f6713e25bbbf86aa27cdb046fd6e8cbaa
START_Eric <start1994eric@gmail.com>
2016-03-19 14:49:09 -0400

    AcmeV2 JFrame created

--- AcmeDistributingV2/src/acme/AcmeDistributingV2.java
+++ AcmeDistributingV2/src/acme/AcmeDistributingV2.java
@@ -5,55 +5,92 @@ import java.awt.*;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 
-/**
- * Created by Xuhao Chen on 16/3/4.
- */
 public class AcmeDistributingV2 extends JFrame implements ItemListener{
 
-    private JTextField date,time,orderNum,itemNum,quantity;
-    private JLabel dateLabel,timeLabel,orderNumLabel,itemNumLabel,quantityLabel;
-    private JTextArea address;
-    private JButton ok,quit;
-
-    public AcmeDistributingV2(){
-        super("Acme Distributing");
-        JPanel fieldPanel = new JPanel(new GridLayout(3,2));
-        JPanel addressPanel = new JPanel(new GridLayout(1,2));
-
-        date = new JTextField(20);
-        time = new JTextField(20);
-        orderNum = new JTextField(20);
-
-        dateLabel = new JLabel("Date");
-        dateLabel.setLabelFor(date);
-        timeLabel = new JLabel("Time");
-        timeLabel.setLabelFor(time);
-        orderNumLabel = new JLabel("Order#");
-        orderNumLabel.setLabelFor(orderNum);
-
-        fieldPanel.add(dateLabel,BorderLayout.NORTH);
-        fieldPanel.add(date);
-        fieldPanel.add(timeLabel);
-        fieldPanel.add(time);
-        fieldPanel.add(orderNumLabel);
-        fieldPanel.add(orderNum);
-
-        add(fieldPanel);
-        add(addressPanel);
-
-        validate();
-
-//        add(date);
-//        add(time);
-    }
+    private JPanel checkedPanel,uncheckedPanel,buttonPanel;
 
 
     public static void main(String[] args){
-        JFrame acme = new AcmeDistributingV2();
-        acme.pack();
-        acme.setVisible(true);
+        JFrame frame;
+        frame = new AcmeDistributingV2();
+
+        frame.pack();
+        int height = frame.getHeight();
+        frame.setPreferredSize(new Dimension(860,height));
+        frame.pack();
+        frame.setVisible(true);
     }
 
+    public AcmeDistributingV2(){
+
+        initPanels();
+
+        addComponentsToChecked();
+        addComponentsToUnchecked();
+        addComponentsToButton();
+
+        add("North",checkedPanel);
+        add("Center",uncheckedPanel);
+        add("South",buttonPanel);
+
+    }
+
+
+    private void initPanels(){
+        checkedPanel = new JPanel();
+        uncheckedPanel = new JPanel();
+        buttonPanel = new JPanel();
+
+        checkedPanel.setLayout(new GridLayout(0,4));
+        uncheckedPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+        buttonPanel.setLayout(new FlowLayout());
+    }
+
+    private void addComponentsToChecked(){
+        JLabel dateLabel, timeLabel, orderLabel, itemLabel, quantityLabel;
+        JTextField dateField, timeField,orderField, itemField, quantityField;
+
+        dateLabel = new JLabel("Date");
+        timeLabel = new JLabel("Time");
+        orderLabel = new JLabel("Order #");
+        itemLabel = new JLabel("Item #");
+        quantityLabel = new JLabel("Quantity");
+
+        dateField = new JTextField();
+        timeField = new JTextField();
+        orderField = new JTextField();
+        itemField = new JTextField();
+        quantityField = new JTextField();
+
+        checkedPanel.add(dateLabel);
+        checkedPanel.add(dateField);
+        checkedPanel.add(timeLabel);
+        checkedPanel.add(timeField);
+        checkedPanel.add(orderLabel);
+        checkedPanel.add(orderField);
+        checkedPanel.add(itemLabel);
+        checkedPanel.add(itemField);
+        checkedPanel.add(quantityLabel);
+        checkedPanel.add(quantityField);
+
+    }
+
+    private void addComponentsToUnchecked(){
+        JLabel addressLabel = new JLabel("Address");
+        JTextArea addressArea = new JTextArea(5,60);
+
+        uncheckedPanel.add(addressLabel);
+        uncheckedPanel.add(addressArea);
+    }
+
+    private void addComponentsToButton(){
+        JButton okButton, quitButton;
+        okButton = new JButton("OK");
+        quitButton = new JButton("Quit");
+
+        buttonPanel.add(okButton);
+        buttonPanel.add(quitButton);
+    }
 
     @Override
     public void itemStateChanged(ItemEvent e) {
